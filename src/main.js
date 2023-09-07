742bbfb301a4fd092fdd0408796c627577cadf79
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2023-09-07 12:32:59 -0600

    Cambios para el main y actualización.
    Agregué nuevos colores a los cards y les di un espacio suficiente para que se vean padres.

--- src/main.js
+++ src/main.js
@@ -18,9 +18,15 @@ function createCards(characters) {
     birthCharacters.textContent = characters.birth;
 
     createCard.classList = 'card';
-    characterName.classList = 'card-title';
-    birthCharacters.classList = 'card-text';
+    characterName.classList = 'card-title text-black fw-700';
+    birthCharacters.classList = 'card-text text-black'; 
     cardBody.classList = 'card-body';
+
+    // Agrega clases personalizadas para el borde redondeado, sombra y borde dorado
+    createCard.classList.add('rounded', 'shadow', 'my-3');
+    createCard.style.borderColor = '#431E15'; 
+    createCard.style.borderWidth = '6px';
+    createCard.style.backgroundColor = '#D9AC36';
     
     cardBody.append(characterName, birthCharacters);
     createCard.append(cardBody);
@@ -39,37 +45,37 @@ data.characters.forEach(oneCharacters => root.appendChild(createCards(oneCharact
 //user story two
 
 
-let filteredPat = filterPatronus(data.characters)
+// let filteredPat = filterPatronus(data.characters)
 
-console.log(filteredPat)
+// console.log(filteredPat)
 
-// create new cards for patronus
-//const createCardP = document.getElementById('patronus')
-//createCardP.addEventListener('click',patronus)
+// // create new cards for patronus
+// //const createCardP = document.getElementById('patronus')
+// //createCardP.addEventListener('click',patronus)
 
-function patronus(filteredPat) {
-    const createCard = document.createElement('div');
-    createCard.classList = 'cards';
-    createCard.classList = 'styleCards';
-    let text = document.createElement('h4')
-    text.textContent = filteredPat
-    createCard.append(text)
-    return createCard
-}
-filteredPat.forEach(oneCharacters => root.appendChild(patronus(oneCharacters)))
+// function patronus(filteredPat) {
+//     const createCard = document.createElement('div');
+//     createCard.classList = 'cards';
+//     createCard.classList = 'styleCards';
+//     let text = document.createElement('h4')
+//     text.textContent = filteredPat
+//     createCard.append(text)
+//     return createCard
+// }
+// filteredPat.forEach(oneCharacters => root.appendChild(patronus(oneCharacters)))
 
-// first: clean dom
-const clean = document.querySelector("main")
-const cleanN = document.getElementById('patronus')
-cleanN.addEventListener('click', () => {
-    clean.innerHTML = " ";
-})
-
-
-//filter by "patronus"
+// // first: clean dom
+// const clean = document.querySelector("main")
+// const cleanN = document.getElementById('patronus')
+// cleanN.addEventListener('click', () => {
+//     clean.innerHTML = " ";
+// })
 
 
-//para filter groups
-const select = document.getElementById("organ").value
-let groups = filterGroups(data.characters, select)
-console.log(groups);
+// //filter by "patronus"
+
+
+// //para filter groups
+// const select = document.getElementById("organ").value
+// let groups = filterGroups(data.characters, select)
+// console.log(groups);
