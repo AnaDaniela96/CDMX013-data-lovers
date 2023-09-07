96ed313fa581e078f890968c846ade0d77ef536a
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2023-09-07 12:07:45 -0600

    Agrega la fecha de nacimiento en los cards.

--- src/main.js
+++ src/main.js
@@ -1,45 +1,68 @@
-import data from './data/harrypotter/data.js';//data de Harry Potter
-import { filterPatronus, filterGroups } from './data.js';
-const root = document.getElementById('texto')
-root.classList = 'card'
+//Imports the date of Harry Potter.
+import data from './data/harrypotter/data.js';
 
-//user story one
-function createCards (characters){
+//Imports the data.js functions to perform the filters
+import { filterPatronus, filterGroups } from './data.js';
+
+//Global functions
+const root = document.getElementById('root');
+
+//Functions
+function createCards(characters) {
     const createCard = document.createElement('div');
-    createCard.classList = 'cards'
-    let text = document.createElement('h4')
-    text.textContent=characters.name 
-    createCard.append(text)
+    const cardBody = document.createElement('div');
+    let characterName = document.createElement('h4');
+    let birthCharacters = document.createElement('p');
+
+    characterName.textContent = characters.name;
+    birthCharacters.textContent = characters.birth;
+
+    createCard.classList = 'card';
+    characterName.classList = 'card-title';
+    birthCharacters.classList = 'card-text';
+    cardBody.classList = 'card-body';
+    
+    cardBody.append(characterName, birthCharacters);
+    createCard.append(cardBody);
     return createCard
 }
-data.characters.forEach(oneCharacters=>root.appendChild(createCards(oneCharacters)))
+data.characters.forEach(oneCharacters => root.appendChild(createCards(oneCharacters)));
 
- //user story two
+
+
+
+
+
+
+
+
+//user story two
 
 
 let filteredPat = filterPatronus(data.characters)
 
 console.log(filteredPat)
 
-   // create new cards for patronus
-    //const createCardP = document.getElementById('patronus')
-    //createCardP.addEventListener('click',patronus)
-    
-function patronus(filteredPat){
+// create new cards for patronus
+//const createCardP = document.getElementById('patronus')
+//createCardP.addEventListener('click',patronus)
+
+function patronus(filteredPat) {
     const createCard = document.createElement('div');
-    createCard.classList = 'cards'
+    createCard.classList = 'cards';
+    createCard.classList = 'styleCards';
     let text = document.createElement('h4')
-    text.textContent=filteredPat
+    text.textContent = filteredPat
     createCard.append(text)
     return createCard
 }
-filteredPat.forEach(oneCharacters=>root.appendChild(patronus(oneCharacters)))
+filteredPat.forEach(oneCharacters => root.appendChild(patronus(oneCharacters)))
 
 // first: clean dom
 const clean = document.querySelector("main")
-const cleanN= document.getElementById('patronus')
-cleanN.addEventListener('click',()=>{
-    clean.innerHTML =" ";  
+const cleanN = document.getElementById('patronus')
+cleanN.addEventListener('click', () => {
+    clean.innerHTML = " ";
 })
 
 
@@ -48,5 +71,5 @@ cleanN.addEventListener('click',()=>{
 
 //para filter groups
 const select = document.getElementById("organ").value
-let groups = filterGroups(data.characters,select)
+let groups = filterGroups(data.characters, select)
 console.log(groups);
